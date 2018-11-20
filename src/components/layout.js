7aef08757ea3c1762408926da71ca719c4980e46
preeme <roa.michael5@gmail.com>
2018-11-20 10:49:14 -0800

    updated index page

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,21 +2,32 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
 
-import Header from './header'
-import './layout.css'
+const MainLayout = styled.main`
+  max-width: 90%;
+  height: 750px;
+  margin: 1rem auto;
+  display: grid;
+  grid-template-columns: auto;
+  grid-gap: 25px;
+  justify-content: center;
+  align-content: center;
+`
+
+const SITE_QUERY = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
+    query={SITE_QUERY}
     render={data => (
       <>
         <Helmet
@@ -28,17 +39,7 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <MainLayout>{children}</MainLayout>
       </>
     )}
   />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,48 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+import { Spring } from 'react-spring';
+import Layout from '../components/layout';
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+const LinkStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 20px;
+  text-align: center;
+  a {
+    font-family: Rock Salt, cursive;
+    font-size: 16pt;
+    text-decoration: none;
+    color: black;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  h1 {
+    font-family: Rock Salt, cursive;
+    font-size: 24pt;
+  }
+
+  @media only screen and (max-width: 1200px) {
+    a {
+      text-decoration: underline;
+    }
+  }
+`;
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Spring from={{ opacity: 0 }} to={{ opacity: 1 }} delay={100}>
+      {props => (
+        <LinkStyles style={props}>
+          <h1>Gutter Nasty</h1>
+          <Link to="/music">Music</Link>
+          <Link to="/barbershop">Barbershop</Link>
+          <Link to="/about">About</Link>
+        </LinkStyles>
+      )}
+    </Spring>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
